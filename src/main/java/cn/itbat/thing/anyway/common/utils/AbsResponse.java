9a3d4dabadf15027d2d6edd47915f99470dd9191
gayhub <gayhub@live.cn>
2018-07-13 09:06:38 +0800

    前端頁面

--- src/main/java/cn/itbat/thing/anyway/common/utils/AbsResponse.java
+++ src/main/java/cn/itbat/thing/anyway/common/utils/AbsResponse.java
@@ -3,15 +3,16 @@ package cn.itbat.thing.anyway.common.utils;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * @author log.r   (;￢＿￢)   
  * @date 2018-07-09 上午9:39
  **/
-public class AbsResponse<T> implements Serializable {
+public class AbsResponse extends HashMap<String, Object> {
 
-    private static final long serialVersionUID = 5014379068811962022L;
+    private static final long serialVersionUID = -8713837118340960775L;
 
     // 成功
     private static final Integer SUCCESS = 0;
@@ -20,135 +21,43 @@ public class AbsResponse<T> implements Serializable {
     // 异常 失败
     private static final Integer FAIL = 500;
 
-    private int code;
-    private String msg;
-    @JsonIgnore
-    private String body;
-    private T data;
-    @JsonIgnore
-    private Map<String, String> params;
-
-
     public AbsResponse() {
-        this(0, (String) null);
+        put("code", SUCCESS);
+        put("msg", "操作成功");
     }
 
-    public AbsResponse(int code, String msg) {
-        this(code, msg, (String) null);
+    public static AbsResponse error(Object msg) {
+        AbsResponse AbsResponse = new AbsResponse();
+        AbsResponse.put("code", FAIL);
+        AbsResponse.put("msg", msg);
+        return AbsResponse;
     }
 
-    public AbsResponse(int code, String msg, String body) {
-        this(code, msg, body, (T) null);
+    public static AbsResponse warn(Object msg) {
+        AbsResponse AbsResponse = new AbsResponse();
+        AbsResponse.put("code", WARN);
+        AbsResponse.put("msg", msg);
+        return AbsResponse;
     }
 
-    public AbsResponse(int code, String msg, String body, T data) {
-        this.code = code;
-        this.msg = msg;
-        this.body = body;
-        this.data = data;
-    }
-
-    public AbsResponse<T> setResult(int code, String msg) {
-        this.code = code;
-        this.msg = msg;
-        return this;
-    }
-
-    public AbsResponse<T> setResult(int code, String msg, String body) {
-        this.code = code;
-        this.msg = msg;
-        this.body = body;
-        return this;
-    }
-
-    public AbsResponse<T> setResult(int code, String msg, String body, T data) {
-        this.code = code;
-        this.msg = msg;
-        this.body = body;
-        this.data = data;
-        return this;
-    }
-
-    public AbsResponse<T> setResult(AbsResponse<?> ano) {
-        return this.setResult(ano.getCode(), ano.getMsg(), ano.getBody());
-    }
-
-    public static AbsResponse error(String msg) {
-        AbsResponse abs = new AbsResponse();
-        abs.setCode(FAIL);
-        abs.setMsg(msg);
-        return abs;
-    }
-
-    public static AbsResponse warn(String msg) {
-        AbsResponse abs = new AbsResponse();
-        abs.setCode(WARN);
-        abs.setMsg(msg);
-        return abs;
-    }
-
-    public static AbsResponse ok(String msg) {
-        AbsResponse abs = new AbsResponse();
-        abs.setCode(SUCCESS);
-        abs.setMsg(msg);
-        return abs;
+    public static AbsResponse ok(Object msg) {
+        AbsResponse AbsResponse = new AbsResponse();
+        AbsResponse.put("code", SUCCESS);
+        AbsResponse.put("msg", msg);
+        return AbsResponse;
     }
 
     public static AbsResponse ok() {
-        AbsResponse abs = new AbsResponse();
-        abs.setCode(SUCCESS);
-        abs.setMsg("操作成功！");
-        return abs;
+        return new AbsResponse();
     }
 
-    @JsonIgnore
-    public boolean isSuccess() {
-        return this.code == 0;
-    }
-
-    public int getCode() {
-        return this.code;
-    }
-
-    public void setCode(int code) {
-        this.code = code;
-    }
-
-    public String getMsg() {
-        return this.msg;
-    }
-
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
-
-    public String getBody() {
-        return this.body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    public T getData() {
-        return this.data;
-    }
-
-    public AbsResponse<T> setData(T data) {
-        this.data = data;
-        return this;
-    }
-
-    public Map<String, String> getParams() {
-        return this.params;
-    }
-
-    public void setParams(Map<String, String> params) {
-        this.params = params;
+    public static AbsResponse error() {
+        return AbsResponse.error("");
     }
 
     @Override
-    public String toString() {
-        return "AbsResponse{code=" + this.code + ", msg='" + this.msg + '\'' + ", data=" + this.data + ", params=" + this.params + '}';
+    public AbsResponse put(String key, Object value) {
+        super.put(key, value);
+        return this;
     }
 }
---
+++ src/main/java/cn/itbat/thing/anyway/controller/RedisController.java
@@ -0,0 +1,151 @@
+package cn.itbat.thing.anyway.controller;
+
+import cn.itbat.thing.anyway.common.domain.RedisInfo;
+import cn.itbat.thing.anyway.common.utils.AbsResponse;
+import cn.itbat.thing.anyway.service.RedisService;
+import com.alibaba.fastjson.JSON;
+import lombok.extern.java.Log;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Set;
+
+@Controller
+@RequestMapping("redis")
+public class RedisController {
+
+    @Autowired
+    private RedisService redisService;
+
+    @RequestMapping("info")
+//    @RequiresPermissions("redis:list")
+    public String getRedisInfo(Model model) {
+        List<RedisInfo> infoList = this.redisService.getRedisInfo();
+        model.addAttribute("infoList", infoList);
+        return "system/redis/info";
+    }
+
+    @RequestMapping("terminal")
+//    @RequiresPermissions("redis:terminal")
+    public String redisTerminal(Model model) {
+        String osName = System.getProperty("os.name");
+        model.addAttribute("osName", osName);
+        return "system/redis/terminal";
+    }
+
+    @RequestMapping("keysSize")
+    @ResponseBody
+    public String getKeysSize() {
+        return JSON.toJSONString(redisService.getKeysSize());
+    }
+
+    @RequestMapping("memeryInfo")
+    @ResponseBody
+    public String getMemeryInfo() {
+        return JSON.toJSONString(redisService.getMemeryInfo());
+    }
+
+    @RequestMapping("keys")
+    @ResponseBody
+    public AbsResponse keys(String arg) {
+        try {
+            Set<String> set = this.redisService.getKeys(arg);
+            return AbsResponse.ok(set);
+        } catch (Exception e) {
+            return AbsResponse.error(e.getMessage());
+        }
+    }
+
+    @RequestMapping("get")
+    @ResponseBody
+    public AbsResponse get(String arg) {
+        try {
+            String result = this.redisService.get(arg);
+            return AbsResponse.ok(result == null ? "" : result);
+        } catch (Exception e) {
+            return AbsResponse.error(e.getMessage());
+        }
+    }
+
+    @RequestMapping("set")
+    @ResponseBody
+    public AbsResponse set(String arg) {
+        try {
+            String args[] = arg.split(",");
+            if (args.length == 1)
+                return AbsResponse.error("(error) ERR wrong number of arguments for 'set' command");
+            else if (args.length != 2)
+                return AbsResponse.error("(error) ERR syntax error");
+            String result = this.redisService.set(args[0], args[1]);
+            return AbsResponse.ok(result);
+        } catch (Exception e) {
+            return AbsResponse.error(e.getMessage());
+        }
+    }
+
+    @RequestMapping("del")
+    @ResponseBody
+    public AbsResponse del(String arg) {
+        try {
+            String args[] = arg.split(",");
+            Long result = this.redisService.del(args);
+            return AbsResponse.ok("(integer) " + result);
+        } catch (Exception e) {
+            return AbsResponse.error(e.getMessage());
+        }
+    }
+
+    @RequestMapping("exists")
+    @ResponseBody
+    public AbsResponse exists(String arg) {
+        try {
+            int count = 0;
+            String arr[] = arg.split(",");
+            for (String key : arr) {
+                if (this.redisService.exists(key))
+                    count++;
+            }
+            return AbsResponse.ok("(integer) " + count);
+        } catch (Exception e) {
+            return AbsResponse.error(e.getMessage());
+        }
+    }
+
+    @RequestMapping("pttl")
+    @ResponseBody
+    public AbsResponse pttl(String arg) {
+        try {
+            return AbsResponse.ok("(integer) " + this.redisService.pttl(arg));
+        } catch (Exception e) {
+            return AbsResponse.error(e.getMessage());
+        }
+    }
+
+    @RequestMapping("pexpire")
+    @ResponseBody
+    public AbsResponse pexpire(String arg) {
+        try {
+            String arr[] = arg.split(",");
+            if (arr.length != 2 || !isValidLong(arr[1])) {
+                return AbsResponse.error("(error) ERR wrong number of arguments for 'pexpire' command");
+            }
+            return AbsResponse.ok("(integer) " + this.redisService.pexpire(arr[0], Long.valueOf(arr[1])));
+        } catch (Exception e) {
+            return AbsResponse.error(e.getMessage());
+        }
+    }
+
+    private static boolean isValidLong(String str) {
+        try {
+            Long.parseLong(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
